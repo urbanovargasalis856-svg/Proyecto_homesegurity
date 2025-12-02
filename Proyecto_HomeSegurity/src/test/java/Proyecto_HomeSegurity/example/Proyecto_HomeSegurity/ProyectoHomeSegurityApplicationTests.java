592a47e682803bd52be81f2dc84460979b958fd4
2006nicolas2023 <149892344+2006nicolas2023@users.noreply.github.com>
2025-12-01 20:01:49 -0500

    Add files via upload

---
+++ Proyecto_HomeSegurity/src/test/java/Proyecto_HomeSegurity/example/Proyecto_HomeSegurity/ProyectoHomeSegurityApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto_HomeSegurity.example.Proyecto_HomeSegurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoHomeSegurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
